e29b41045084cca20ec9570deb6206bb10a564bc
trungtm <trungtm@bit-vietnam.com>
2019-08-07 18:08:35 +0700

    Responsive

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Button, StyleSheet, ImageBackground } from "react-native";
+import { View, StyleSheet, ImageBackground, Dimensions } from "react-native";
 
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 import HeadingText from "../../components/UI/HeadingText/HeadingText";
@@ -9,21 +9,50 @@ import backgroundImage from '../../../assets/logo.jpg';
 
 
 class AuthScreen extends Component {
+  getDeviceDims = dims => dims.height > 500 ? 'portrait' : 'landscape'
+  
+  state = {
+    deviceDims: this.getDeviceDims(Dimensions.get('window'))
+  }
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener('change', this.deviceRotateHandler);
+  }
+
+  deviceRotateHandler = dims => {
+    this.setState({
+      deviceDims: this.getDeviceDims(dims.window)
+    });
+  }
+
+  
   render() {
+    headingContent = null
+    if(this.state.deviceDims === "portrait") {
+      headingContent = (
+      <MainText>
+        <HeadingText>Please Log In</HeadingText>
+      </MainText>
+      );
+    }
     return (
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
         <View style={styles.authContainer}>
-          <MainText>
-            <HeadingText>Please Log In</HeadingText>
-          </MainText>
+          {headingContent}
           <CustomButton onPress={() => {alert('sss')}} color="#9c4dcc">Switch to Login</CustomButton>
           <View style={styles.inputContainer}>
             <DefaultInput
               placeholder="Your Email Address"
               style={styles.input}
             />
-            <DefaultInput placeholder="Password" style={styles.input} />
-            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+            <View style={this.state.deviceDims === "portrait" ? styles.portraitPasswordContainer : styles.landscapePasswordContainer}>
+              <View style={this.state.deviceDims === "portrait" ? styles.portraitPasswordInput : styles.landscapePasswordInput}>
+                <DefaultInput placeholder="Password" style={styles.input} />
+              </View>
+              <View style={this.state.deviceDims === "portrait" ? styles.portraitPasswordInput : styles.landscapePasswordInput}>
+                <DefaultInput placeholder="Confirm Password" style={styles.input} />
+              </View>
+            </View>
           </View>
           <CustomButton onPress={() => {this.props.navigation.navigate('Main')}} color="#9c4dcc">Submit</CustomButton>
         </View>
@@ -49,6 +78,20 @@ const styles = StyleSheet.create({
       flex: 1,
       width: "100%",
       
+  }, 
+  portraitPasswordContainer: {
+    flexDirection: "column",
+    justifyContent: "flex-start"
+  },
+  landscapePasswordContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  portraitPasswordInput: {
+    width: "100%"
+  },
+  landscapePasswordInput: {
+    width: "45%"
   }
 });
 export default AuthScreen;

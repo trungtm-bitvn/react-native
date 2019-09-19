cb269a7842257d61cbe5cb4a461ce0273c93420c
trungtm <trungtm@bit-vietnam.com>
2019-09-19 18:13:18 +0700

    getExpoPushToken

--- src/screens/Drawer/Drawer.js
+++ src/screens/Drawer/Drawer.js
@@ -5,10 +5,49 @@ import { Ionicons } from '@expo/vector-icons';
 import { connect } from 'react-redux';
 import { logout } from '../../store/actions/index'
 
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
+
 class Drawer extends Component {
     static navigationOptions = {
         title: 'Find Places',
     }
+
+    getExpoToken = async () => {
+        const { status: existingStatus } = await Permissions.getAsync(
+            Permissions.NOTIFICATIONS
+        );
+        let finalStatus = existingStatus;
+
+        // only ask if permissions have not already been determined, because
+        // iOS won't necessarily prompt the user a second time.
+        if (existingStatus !== "granted") {
+            // Android remote notification permissions are granted during the app
+            // install, so this will only ask on iOS
+            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+            finalStatus = status;
+        }
+
+        // Stop here if the user did not grant permissions
+        if (finalStatus !== "granted") {
+            return;
+        }
+
+        // Get the token that uniquely identifies this device
+        let token = await Notifications.getExpoPushTokenAsync();
+
+        return fetch("https://reactnativelearning-50c20.firebaseio.com/token.json", {
+            method: 'POST',
+            body: JSON.stringify({
+                token: {
+                    value: token,
+                },
+                user: {
+                    username: 'Brent',
+                },
+            }),
+        });
+    };
     logout = () => {
         this.props.logoutHandler();
         this.props.navigation.navigate('Auth');
@@ -24,6 +63,10 @@ class Drawer extends Component {
                 </TouchableOpacity>
             </View>
         );
+    }
+
+    componentDidMount() {
+        this.getExpoToken();
     }
     
 }
